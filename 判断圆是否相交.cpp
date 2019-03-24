a79b2d28cce69fa32ec707101ba8cf2a0e4f399e
shiina mayuri <xydreamorigin@gmail.com>
2019-03-24 14:42:30 +0800

    类实现

---
+++ 判断圆是否相交.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class point
+{
+	public:
+		point()
+		{}
+		~point()
+		{}
+		float x=0.0;
+		float y=0.0;
+		float dis(point x1,point y1)
+		{
+			return sqrt((x1.x-y1.x)*(x1.x-y1.x)+(x1.y-y1.y)*(x1.y-y1.y));
+		}
+};
+
+class circle
+{	
+	public:
+		float radius;
+		point O;
+		void get()
+		{
+			cout<<"请输入圆心坐标："<<endl;
+			cin>>O.x>>O.y;
+			cout<<"请输入圆的半径："<<endl;
+			cin>>radius;
+		}
+};
+
+int main()
+{
+	circle O1,O2;
+	float res;
+	cout<<"请输入两个圆的信息:"<<endl;
+	O1.get();
+	O2.get(); 
+	res=O1.O.dis(O1.O,O2.O);
+	if(res>O1.radius+O2.radius||res<O1.radius-O2.radius)
+		cout<<"两个圆不相交"<<endl;
+	else
+		cout<<"两个圆相交"<<endl;
+		
+	system("pause");
+	return 0; 
+ } 
+
